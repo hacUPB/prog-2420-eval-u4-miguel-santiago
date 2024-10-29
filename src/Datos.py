081baaccbd49f3b7f3b756fb6a7c7733c5e5eb33
miguelangelpz2006 <miguel.perea@upb.edu.co>
2024-10-28 19:14:24 -0500

    primer avance del menú

--- src/Datos.py
+++ src/Datos.py
@@ -1,7 +1,18 @@
-
+import os
 def main():
-    #Tu código va aquí. Mantén la indentación
-    pass # borra esta línea cuando con inicies tu código
+    #Menú principal
+    print(" \n --- MENÚ ---")
+    print("1--Listar archivos presentes en la ruta actual o ingresar una ruta donde buscar los archivos.")
+    print("2--Procesar archivo de texto (.txt)")
+    print("3--Procesar archivo separado por comas (.csv)")
+    print("4--Salir")
+    opcion1 = int(input("ingrese su opción"))
+    while True:
+        if opcion1 == 1:
+            print("Haz seleccionado: listar archivos presentes en la ruta actual o ingresar una ruta donde buscar los archivos")
+            ##LLamar funcion para la ruta de los archivos
+            
+
 
 
 if __name__ == "__main__":
