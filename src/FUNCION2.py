216c650cc1e5e2754df4a1d84bacd7abe2221c07
miguelangelpz2006 <miguel.perea@upb.edu.co>
2024-10-31 10:26:27 -0500

    FUNCION 2 DE LA FUNCION 2
    Hice la función 2  de la función 2 y queda pendiente la función 2,1

--- src/FUNCION2.py
+++ src/FUNCION2.py
@@ -2,10 +2,10 @@
 #Contar número de palabras**: Debe contar el número de palabras en el archivo de texto y mostrar el resultado.
 #Reemplazar una palabra por otra**: Debe permitir al usuario reemplazar una palabra por otra en el archivo de texto. La función debe recibir tanto la palabra a buscar, como la palabra por la que se tiene que reemplazar.
 
-def contar_palabras():
+def contar_palabras():  ##Arreglar
     filename = input("Ingrese la dirección del texto (no olvidar poner .txt): ")
     try:
-        with open(filename, 'r') as f_obj:
+        with open(filename, 'r+') as f_obj:
             contenido = f_obj.read()
     except FileNotFoundError:
         print(f"El archivo {filename} no existe.")
@@ -13,5 +13,33 @@ def contar_palabras():
         palabras = contenido.split()
         numero_de_palabras = len(palabras)
         print(f"Número de palabras en el archivo: {numero_de_palabras}")
-
+        return
+    
 contar_palabras()
+
+
+
+
+def reemplazar_palabra():
+    filename = input("Ingrese la dirección del texto (no olvidar poner .txt): ")
+    
+    try:
+        with open(filename, 'r+') as txtfile:
+            contenido = txtfile.read()
+    except FileNotFoundError:
+        print(f"El archivo {filename} no existe.")
+        return
+
+    palabras = contenido.split()
+
+    vieja_palabra = input("Ingrese la palabra a reemplazar: ")
+    nueva_palabra = input("Ingrese la nueva palabra: ")
+
+
+    nuevo_contenido = contenido.replace(vieja_palabra, nueva_palabra)
+    
+
+    print("Contenido modificado:")
+    print(nuevo_contenido)
+
+reemplazar_palabra()
