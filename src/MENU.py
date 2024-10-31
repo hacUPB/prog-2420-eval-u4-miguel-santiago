a1acc445fb432b3b4f21b191ab899ed3e83de6a6
santivera111 <santivera111@gmail.com>
2024-10-30 19:19:00 -0500

    Update MENU.py
    Realizo inicialmente el primer intento del codigo del menu y submenu con diferentes funciones tanto para generar menu, leer menu, y ejecturar el menu junto con un bucle para corregir el error de que digiten mal la opcion

--- src/MENU.py
+++ src/MENU.py
@@ -1,1 +1,26 @@
-Menu
+def mostrar_menu(opciones):
+    print("Seleccione una opcion")
+    for i in opciones:
+        print(f"{i}) {opciones[i][0]}")
+
+def leer_opcion():
+
+    Numero = input("Opcion :")
+
+    while Numero not in opciones :
+        print("Opcion no valida, regresando al inicio ")
+
+    return Numero
+
+def ejecutar_menu(opcion, opciones):
+    opciones[opcion][1]()    #Buscar explicacion bien
+
+def menu()
+    opciones = {
+        "1": ("Opc1":"eleg1")
+        "2": ("Opc2":"eleg2")
+        "3": ("Opc3":"eleg3")
+        "4": ("Opc4":"eleg4")
+        }
+
+    generar_menu(opciones, "4")
