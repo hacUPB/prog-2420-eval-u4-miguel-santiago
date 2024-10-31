a72935b6fc9870361900be9dbcaf046bff620fa8
santivera111 <santivera111@gmail.com>
2024-10-31 10:27:08 -0500

    Update MENU.py
    actualizocodigo con el menu realizado

--- src/MENU.py
+++ src/MENU.py
@@ -1,26 +1,69 @@
-def mostrar_menu(opciones):
-    print("Seleccione una opcion")
-    for i in opciones:
-        print(f"{i}) {opciones[i][0]}")
 
-def leer_opcion():
 
-    Numero = input("Opcion :")
+def new_func():
+    def mostrar_menu(opciones):
+        print("Seleccione una opcion")
+        for i in opciones:
+            print(f"{i}) {opciones[i][0]}") 
 
-    while Numero not in opciones :
-        print("Opcion no valida, regresando al inicio ")
+        
 
-    return Numero
 
-def ejecutar_menu(opcion, opciones):
-    opciones[opcion][1]()    #Buscar explicacion bien
 
-def menu()
-    opciones = {
-        "1": ("Opc1":"eleg1")
-        "2": ("Opc2":"eleg2")
-        "3": ("Opc3":"eleg3")
-        "4": ("Opc4":"eleg4")
+    def leer_opcion(opciones):
+        Numero = input("Digite su opcion : ")
+        while Numero not in opciones:
+            print("Opcion no valida, regresando al inicio") 
+            Numero = input("Digite su opcion : ")
+        return Numero
+
+    def ejecutar_menu(opcion, opciones): 
+        opciones[opcion][1]()    #Opcion siendo la clave del diccionario donde buscaremos la informacio
+                            #Opciones es el nombre del diccionario donde la clave tiene una cadena y el valor es una tupla donde el primer elemento es la opcion y el 
+                            #segundo es la funcion que se ejecutara si el cliente selecciono esa opcion
+
+    def generar_menu(opciones,opcion_salida):
+        opcion = None
+        while opcion != opcion_salida:
+            mostrar_menu(opciones)
+            opcion = leer_opcion(opciones)
+            ejecutar_menu(opcion,opciones)
+            print()
+
+
+
+    def menu():
+        opciones = {
+        "1": ("Listar archivos o conocer la ruta para buscarlos",eleg1),
+        "2": ("Procesar archivos .txt",eleg2),
+        "3": ("Procesar archivos .csv",eleg3),
+        "4": ("Salir",salida)
         }
+        generar_menu(opciones, "4")
+    
 
-    generar_menu(opciones, "4")
+
+
+
+
+    def eleg1():
+        print("Elegiste la opcion 1")
+
+    def eleg2():
+         print("Elegiste la opcion 2")
+
+    def eleg3():
+         print("Elegiste la opcion 3")
+
+    def salida():
+         print("Saliendo")
+
+
+
+
+
+
+    if __name__=="__main__":
+        menu()
+
+return new_func()
