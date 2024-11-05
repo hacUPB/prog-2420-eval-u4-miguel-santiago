3d3915a7b10174a37bb412715888b26f2bfdb755
santivera111 <santivera111@gmail.com>
2024-11-05 08:43:54 -0500

    FUNCION2.py
    Realizo el cambio para llamar la funcion contar palabras

--- src/FUNCION2.py
+++ src/FUNCION2.py
@@ -2,7 +2,9 @@
 #Contar número de palabras**: Debe contar el número de palabras en el archivo de texto y mostrar el resultado.
 #Reemplazar una palabra por otra**: Debe permitir al usuario reemplazar una palabra por otra en el archivo de texto. La función debe recibir tanto la palabra a buscar, como la palabra por la que se tiene que reemplazar.
 
-def contar_palabras():  ##Arreglar
+
+
+def contar_palabras():
     filename = input("Ingrese la dirección del texto (no olvidar poner .txt): ")
     try:
         with open(filename, 'r') as f_obj:
@@ -13,10 +15,10 @@ def contar_palabras():  ##Arreglar
         palabras = contenido.split()
         numero_de_palabras = len(palabras)
         print(f"Número de palabras en el archivo: {numero_de_palabras}")
-        return
-    
-contar_palabras()
 
+# Este bloque evita que la función se ejecute al importar el módulo
+if __name__ == "__main__":
+    contar_palabras()
 
 
  
