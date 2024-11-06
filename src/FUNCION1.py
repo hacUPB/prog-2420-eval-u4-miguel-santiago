c1b25a0d9381f1edf7b17697c136c561a52b672e
miguelangelpz2006 <miguel.perea@upb.edu.co>
2024-11-06 18:23:54 -0500

    FUNCION 1 COMPLETA

--- src/FUNCION1.py
+++ src/FUNCION1.py
@@ -1,1 +1,13 @@
-Def
+def listar_archivos_en_directorio():
+
+    ruta_directorio = os.path.dirname(os.path.abspath(__file__))
+    
+    archivos = os.listdir(ruta_directorio)
+
+    if archivos:
+        print("ARCHIVOS EN EL DIRECTORIO ACTUAL:")
+        for archivo in archivos:
+            print(f"-{archivo}")
+            print()
+    else:
+        print("No se encontraron archivos en el directorio.")
